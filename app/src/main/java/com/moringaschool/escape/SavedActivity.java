eb99ceceb857a9b1b026a49c126f3c35d7c8fdb0
Agnes Nyiramisigaro <ruzindanawendy@gmail.com>
2019-10-13 18:57:26 +0200

    added textviews for all the texts user enters in order for them to be displayed

--- app/src/main/java/com/moringaschool/escape/SavedActivity.java
+++ app/src/main/java/com/moringaschool/escape/SavedActivity.java
@@ -1,4 +1,40 @@
 package com.moringaschool.escape;
 
-public class SavedActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.jar.Attributes;
+
+public class SavedActivity extends AppCompatActivity {
+    private TextView mlocationNameText;
+    private TextView mlocationTypeText;
+    private TextView mlocationDescriptionText;
+    private TextView mlocationDresscodeText;
+    private TextView mlocationTimeText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved);
+        mlocationNameText = (TextView) findViewById(R.id.locationNameText);
+        mlocationTypeText=(TextView) findViewById(R.id.locationTypeText);
+        mlocationDescriptionText=(TextView) findViewById(R.id.locationDescriptionText);
+        mlocationDresscodeText=(TextView) findViewById(R.id.locationDressCodeText);
+        mlocationTimeText=(TextView) findViewById(R.id.locationTimeText);
+        Intent intent2 = getIntent();
+        String Name = intent2.getStringExtra("Name");
+        System.out.println(Name);
+        String Type = intent2.getStringExtra("Type");
+        String Description = intent2.getStringExtra("Description");
+        String Dresscode = intent2.getStringExtra("Dresscode");
+        String Time = intent2.getStringExtra("Time");
+        mlocationNameText.setText(  Name);
+        mlocationTypeText.setText(  Type);
+        mlocationDescriptionText.setText(  Description);
+        mlocationDresscodeText.setText(  Dresscode);
+        mlocationTimeText.setText(  Time);
+    }
 }
