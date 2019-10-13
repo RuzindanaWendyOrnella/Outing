15fedd09dbab5d09334ccaa6774061fb983fe2a5
Agnes Nyiramisigaro <ruzindanawendy@gmail.com>
2019-10-13 15:55:36 +0200

    created and updated event class

--- app/src/main/java/com/moringaschool/escape/EventActivity.java
+++ app/src/main/java/com/moringaschool/escape/EventActivity.java
@@ -1,4 +1,16 @@
 package com.moringaschool.escape;
 
-public class EventActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+
+    }
+
 }
