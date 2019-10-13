81c7587fa526683e3fbda2e276ba8787604c6fa9
Agnes Nyiramisigaro <ruzindanawendy@gmail.com>
2019-10-13 17:15:45 +0200

    updated escape activity class with an intent

--- app/src/main/java/com/moringaschool/escape/EscapeActivity.java
+++ app/src/main/java/com/moringaschool/escape/EscapeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class EscapeActivity extends AppCompatActivity {
     private Button mEventButton;
-
+    private EditText mlocationNameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escape);
         mEventButton = (Button) findViewById(R.id.eventButton);
+        mlocationNameText = (EditText) findViewById(R.id.locationNameText);
         mEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
