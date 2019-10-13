c97d6a0d2e35b4dff4ef14e6a3c1159956d3a232
Agnes Nyiramisigaro <ruzindanawendy@gmail.com>
2019-10-13 17:17:40 +0200

    updated event class with edit text for name

--- app/src/main/java/com/moringaschool/escape/EventActivity.java
+++ app/src/main/java/com/moringaschool/escape/EventActivity.java
@@ -1,16 +1,17 @@
 package com.moringaschool.escape;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventActivity extends AppCompatActivity {
-
+    private EditText mlocationNameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
-
+        mlocationNameText = (EditText) findViewById(R.id.locationNameText);
     }
 
 }
